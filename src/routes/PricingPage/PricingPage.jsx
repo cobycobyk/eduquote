a891c319157b2be6ae501183f61badff3da449a3
Coby Kabili <ckabili8@gmail.com>
2023-01-17 12:58:16 -0700

    pricing page styling

--- src/routes/PricingPage/PricingPage.jsx
+++ src/routes/PricingPage/PricingPage.jsx
@@ -1,15 +1,55 @@
 import React, { useState } from "react";
-import { PCPrice, PCTitle, PricingCard, PricingCardContainer, PricingContainer, PricingSubtitle, PricingTitle, TimeToggle, Toggle, ToggleButton } from "./PricingPage.styles";
-import { priceFormatter } from "../../utils/helperFunctions/PriceFormatter";
+import { Discount, PCFeature, PCFeatures, PCFeatureText, PCPrice, PCTitle, PricingCard, PricingCardContainer, PricingContainer, PricingSubtitle, PricingTitle, TimeToggle, Toggle, ToggleButton } from "./PricingPage.styles";
+import * as Icon from "react-feather";
 
 const pricingOptionsMonthly = [
-  {id: 1, name: "Starter", price: 29, features: ["Create Unlimited Quotes", "Create Personalized Quotes"]},
-  {id: 2, name: "Pro", price: 39, features: ["Create Unlimited Quotes", "Create Personalized Quotes"]}
-]
+  {
+    id: 1,
+    name: "Starter",
+    price: 29,
+    features: [
+      "Create Unlimited Quotes",
+      "Create Personalized Quotes",
+      "Create Unlimited Quotes",
+      "Create Personalized Quotes",
+    ],
+  },
+  {
+    id: 2,
+    name: "Pro",
+    price: 39,
+    features: [
+      "Create Unlimited Quotes",
+      "Create Personalized Quotes",
+      "Create Unlimited Quotes",
+      "Create Personalized Quotes",
+    ],
+  },
+];
 const pricingOptionsYearly = [
-  {id: 1, name: "Starter", price: 300, features: ["Create Unlimited Quotes", "Create Personalized Quotes"]},
-  {id: 2, name: "Pro", price: 400, features: ["Create Unlimited Quotes", "Create Personalized Quotes"]}
-]
+  {
+    id: 1,
+    name: "Starter",
+    price: 300,
+    features: [
+      "Create Unlimited Quotess",
+      "Create Personalized Quotes",
+      "Create Unlimited Quotess",
+      "Create Personalized Quotes",
+    ],
+  },
+  {
+    id: 2,
+    name: "Pro",
+    price: 400,
+    features: [
+      "Create Unlimited Quotes",
+      "Create Personalized Quotes",
+      "Create Unlimited Quotes",
+      "Create Personalized Quotes",
+    ],
+  },
+];
 
 export default function PricingPage() {
   const [timeOption, setTimeOption] = useState(true);
@@ -25,20 +65,40 @@ export default function PricingPage() {
           <ToggleButton></ToggleButton>
         </Toggle>
         <div>Yearly</div>
-        <div>20%off</div>
+        <Discount>20%off</Discount>
       </TimeToggle>
       <PricingCardContainer>
         {timeOption
           ? pricingOptionsMonthly.map((option, key) => (
-              <PricingCard>
+              <PricingCard key={key}>
                 <PCTitle>{option.name}</PCTitle>
                 <PCPrice>${option.price}</PCPrice>
+                <PCFeatures>
+                  {option.features.map((feature, key) => {
+                    return (
+                      <PCFeature key={key}>
+                        <Icon.Check />
+                        <PCFeatureText>{feature}</PCFeatureText>
+                      </PCFeature>
+                    );
+                  })}
+                </PCFeatures>
               </PricingCard>
             ))
           : pricingOptionsYearly.map((option, key) => (
-              <PricingCard>
+              <PricingCard key={key}>
                 <PCTitle>{option.name}</PCTitle>
                 <PCPrice>${option.price}</PCPrice>
+                <PCFeatures>
+                  {option.features.map((feature, key) => {
+                    return (
+                      <PCFeature key={key}>
+                        <Icon.Check />
+                        <PCFeatureText>{feature}</PCFeatureText>
+                      </PCFeature>
+                    );
+                  })}
+                </PCFeatures>
               </PricingCard>
             ))}
       </PricingCardContainer>
--- src/routes/PricingPage/PricingPage.styles.jsx
+++ src/routes/PricingPage/PricingPage.styles.jsx
@@ -15,6 +15,7 @@ export const PricingTitle = styled.h3``;
 export const PricingSubtitle = styled.h6``;
 export const TimeToggle = styled.div`
   display: flex;
+  align-items: center;
 `;
 export const Toggle = styled.div`
   display: flex;
@@ -24,6 +25,7 @@ export const Toggle = styled.div`
   border: 1px solid black;
   border-radius: 20px;
   width: 3.5vw;
+  height: 1.5vw;
   :hover {
     cursor: pointer;
   }
@@ -33,13 +35,21 @@ export const ToggleButton = styled.div`
   border-radius: 20px;;
   background-color: orange;
 `;
+export const Discount = styled.div`
+  color: orange;
+  margin-left: 5px;
+  border: 1px solid orange;
+  border-radius: 1rem;
+  font-size: 0.75rem;
+  padding: 0.25rem;
+`;
 export const PricingCardContainer = styled.div`
   display: flex;
 `;
 export const PricingCard = styled.div`
   display: flex;
   flex-direction: column;
-  width: 10vw;
+  width: 20vw;
   border: 1px solid black;
   border-radius: 10px;
   margin: 10px;
@@ -52,4 +62,15 @@ export const PCTitle = styled.div`
 export const PCPrice = styled.div`
   font-weight: bold;
   color: grey;
+`;
+export const PCFeatures = styled.div`
+  align-items: start;
+  margin-top: 10px;
+  margin-bottom: 10px;
+`;
+export const PCFeature = styled.div`
+  display: flex;
+`;
+export const PCFeatureText = styled.div`
+  margin-left: 5px;
 `;
