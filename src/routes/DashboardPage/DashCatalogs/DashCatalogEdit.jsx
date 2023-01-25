d3aa4e3d065526fc0e329a96ca507a97951cfa3d
Coby Kabili <ckabili8@gmail.com>
2023-01-25 14:53:20 -0700

    edit catalog categories and sub categories

--- src/routes/DashboardPage/DashCatalogs/DashCatalogEdit.jsx
+++ src/routes/DashboardPage/DashCatalogs/DashCatalogEdit.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import {
   CardTitlee,
@@ -10,31 +10,76 @@ import {
   SignupInput,
   SignupLabelRow,
   SignupRow,
+  SignupSelect,
 } from "../../SignupPage/SignupPage.styles";
 import * as Icon from "react-feather";
 import { CancelButton, Danger, SaveButton } from "../../../assets/css/custom.styles";
-import { deleteCatalog, updateCatalog } from "../../../utils/firebase";
+import { addCatalogCategory, addCatalogSubCategory, deleteCatalog, updateCatalog } from "../../../utils/firebase";
+import { ProductsContext } from "../../../context/products.context";
+import { UserContext } from "../../../context/user.context";
 
 export default function DashCatalogEdit({setCurrentPage}) {
   const [formData, setFormData] = useState({});
   const location = useLocation();
   const catalog = location.state?.data
   const navigate = useNavigate();
+  const { currentUserInfo } = useContext(UserContext);
+  const { catalogCategories } = useContext(ProductsContext);
+  const [newCategory, setNewCategory] = useState(false);
+  const [newSubCategory, setNewSubCategory] = useState({ name: "", parent: "" });
+  const [message, setMessage] = useState(false);
+  const [catalogSubCategories, setCatalogSubCategories] = useState([]);
 
   useEffect(() => {
     setFormData(catalog);
     setCurrentPage(`Edit ${catalog.name} Catalog`);
   }, []);
+  useEffect(() => {
+    const subCats = () => {
+      const a = [];
+      const subs = catalogCategories.filter((cat) => {
+        if (cat.name === formData.category) {
+          console.log(cat.subCategories)
+          return cat.subCategories;
+        }
+      })
+      setCatalogSubCategories(subs[0]?.subCategories);
+    }
+    catalogCategories && subCats();
+  }, [formData.category])
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
+    if (name === "category" && value === "new") {
+      setFormData({ ...formData, [name]: "" });
+      return setNewCategory(true);
+    } else if (name === "subCategory") {
+      setNewSubCategory({
+        ...newSubCategory,
+        name: value,
+        parent: formData.category,
+      });
+      setFormData({ ...formData, [name]: value });
+    } else {
+      setFormData({ ...formData, [name]: value });
+    }
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    if (!formData.category.length)
+      return setMessage("Please enter category name");
+    if (catalogCategories.some((cat) => cat.name === newCategory))
+      return setMessage("Category already exists");
+    if (newCategory) {
+      await addCatalogCategory(currentUserInfo, formData.category);
+    }
+    if (newSubCategory) {
+      await addCatalogSubCategory(currentUserInfo, newSubCategory)
+    }
     await updateCatalog(formData);
     navigate(`/dashboard/catalogs`);
+    
   };
 
   const handleDelete = async (e) => {
@@ -47,6 +92,7 @@ export default function DashCatalogEdit({setCurrentPage}) {
     <React.Fragment>
       <SignupCard>
         <CardTitlee>Edit Catalog Information</CardTitlee>
+        {message && <div>{message}</div>}
         <Formm onSubmit={handleSubmit}>
           <SignupRow>
             <SignupColumn>
@@ -80,22 +126,42 @@ export default function DashCatalogEdit({setCurrentPage}) {
                   Category <Danger>*</Danger>
                 </FormLabel>
               </SignupLabelRow>
-              <SignupInput
-                type="text"
-                name="category"
+              <SignupSelect
                 value={formData.category}
+                name="category"
                 onChange={handleChange}
-                id="category"
-                placeholder="Category"
-                required
+                id="catgeory"
+                placeholder="New Category Name"
                 errorMessage=""
-                validate={{
-                  required: {
-                    value: true,
-                    errorMessage: "Please enter a category",
-                  },
-                }}
-              />
+              >
+                <option value={formData.category} selected>
+                  {formData.category
+                    ? formData.category
+                    : "Type new Category below"}
+                </option>
+                {catalogCategories?.map((cat, key) => {
+                  return <option key={key}>{cat.name}</option>;
+                })}
+                <option value="new">Enter New Category</option>
+              </SignupSelect>
+              {newCategory && (
+                <SignupInput
+                  type="text"
+                  name="category"
+                  value={formData.category}
+                  onChange={handleChange}
+                  id="category"
+                  placeholder="Category"
+                  required
+                  errorMessage=""
+                  validate={{
+                    required: {
+                      value: true,
+                      errorMessage: "Please enter a category",
+                    },
+                  }}
+                />
+              )}
             </SignupColumn>
           </SignupRow>
           <SignupRow>
@@ -156,9 +222,7 @@ export default function DashCatalogEdit({setCurrentPage}) {
           </SignupRow>
         </Formm>
       </SignupCard>
-      <SignupCard>
-
-      </SignupCard>
+      <SignupCard></SignupCard>
     </React.Fragment>
   );
 }
